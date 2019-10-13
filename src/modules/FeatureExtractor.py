8b33db6b810c99fb777802b86b769619007239ef
Bo <bobracquez@gmail.com>
2019-10-13 20:36:01 +0200

    feat(): FeatureExtractor ModelFactory
    Code implementation

---
+++ src/modules/FeatureExtractor.py
@@ -0,0 +1,87 @@
+import librosa
+import numpy
+
+class FeatureExtractor():
+    def loadFile(self, file, duration, offset):
+        self.file = file
+        self.res_type = 'kaiser_best'
+
+        # Load the file in mono format, for a (duration) timespan and start at timeframe a (to prevent intros misbehaving + generate multiple datasets)
+        self.y, self.sr = librosa.load(file, mono=True, duration=duration, offset=offset, sr=22050,
+                             res_type=self.res_type)
+
+    def y(self, y):
+        self.y = y
+
+    def sr(self, sr):
+        self.sr = 22050
+
+    def extract(self):
+        out = {}
+
+        if self.file is not None:
+            out = {
+                'arousal': 0,
+                'valence': 0,
+            }
+
+        # Compute the spectogram
+        self.S = librosa.stft(self.y)
+        self.S_magphase, phase = librosa.magphase(self.S)
+        self.onset_env = librosa.onset.onset_strength(y=self.y, sr=self.sr)
+        self.chroma_stft = librosa.feature.chroma_stft(S=numpy.abs(self.S), sr=self.sr)
+
+        # The following features are based on https://iopscience.iop.org/article/10.1088/1757-899X/482/1/012019/pdf
+
+        # Zero Crossing Rate
+        #   Librosa: https://librosa.github.io/librosa/generated/librosa.feature.zero_crossing_rate.html
+        out['zero_crossing_rate'] = numpy.mean(librosa.feature.zero_crossing_rate(y=self.y))
+
+        # Energy
+        #   Librosa: https://librosa.github.io/librosa/generated/librosa.feature.rms.html
+        out['energy'] = numpy.mean(librosa.feature.rms(y=self.y))
+
+        # Entropy of Energy
+        #   Librosa: http://librosa.github.io/librosa/generated/librosa.feature.spectral_contrast.html
+        self.entropy_of_energy = librosa.feature.spectral_contrast(S=numpy.abs(S), sr=self.sr)
+        out['entropy_of_energy'] = numpy.mean(self.entropy_of_energy)
+        out['entropy_of_energy_std'] = numpy.std(self.entropy_of_energy)
+
+        # Spectral Energy
+        #   Librosa: http://librosa.github.io/librosa/generated/librosa.feature.spectral_bandwidth.html
+        self.spectral_energy = librosa.feature.spectral_bandwidth(S=self.S_magphase)
+        out['spectral_energy'] = numpy.mean(self.spectral_energy)
+        out['spectral_energy_std'] = numpy.std(self.spectral_energy)
+
+        # Spectral Flux
+        #   Librosa: http://librosa.github.io/librosa/generated/librosa.onset.onset_strength.html?highlight=onset_strength#librosa.onset.onset_strength
+        out['spectral_flux'] = numpy.mean(self.onset_env)
+
+        # Spectral Roll-off
+        #   Librosa: http://librosa.github.io/librosa/generated/librosa.feature.spectral_rolloff.html
+        out['spectral_rolloff'] = numpy.mean(librosa.feature.spectral_rolloff(S=self.S_magphase, sr=self.sr))
+
+        # MFCC
+        #   Librosa: http://librosa.github.io/librosa/generated/librosa.feature.mfcc.html
+        counter = 1
+        for mfcc in (librosa.feature.mfcc(y=self.y, sr=self.sr, n_mels=13)):
+            out[('mfcc%s' % counter)] = numpy.mean(mfcc)
+            counter = counter + 1
+
+        # Chroma Vector (Pitch vector)
+        #   Librosa: http://librosa.github.io/librosa/generated/librosa.feature.chroma_stft.html
+        counter = 1
+        for chroma in (self.chroma_stft):
+            out[('chroma_vector%s' % counter)] = numpy.mean(chroma)
+            counter = counter + 1
+
+        # Chroma Deviation (Pitch deviation)
+        #   Librosa: http://librosa.github.io/librosa/generated/librosa.feature.chroma_stft.html
+        out['chroma_deviation'] = numpy.std(self.chroma_stft)
+
+        # Tempo
+        #   Librosa: http://librosa.github.io/librosa/generated/librosa.feature.tempogram.html#librosa.feature.tempogram
+        #   Librosa: http://librosa.github.io/librosa/generated/librosa.beat.plp.html#librosa.beat.plp
+        out['tempo'] = numpy.mean(librosa.beat.tempo(onset_envelope=self.onset_env, sr=self.sr))
+
+        return out
---
+++ src/modules/ModelFactory.py
@@ -0,0 +1,126 @@
+from sklearn.utils import shuffle
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.externals import joblib
+import pandas
+import numpy
+import tensorflow
+import os
+from tensorflow.keras import models
+from tensorflow.keras import layers
+
+class ModelFactory():
+    def __init__(self, datadir, datafile):
+        self.datadir = datadir
+        self.datafile = datafile
+
+    def pathFormat(self, type, extension):
+        return '%s/%s.%s' % (self.datadir, type, extension)
+
+    def loadScaler(self, type):
+        # Create scalers (load the from saved scaler files if they exist)
+        return (joblib.load(self.pathFormat(type, 'scalerX')) if os.path.isfile(self.pathFormat(type, 'scalerX')) else MinMaxScaler(),
+                joblib.load(self.pathFormat(type, 'scalerY')) if os.path.isfile(self.pathFormat(type, 'scalerY')) else MinMaxScaler())
+
+    def saveScaler(self, type):
+        # Save the scales
+        joblib.dump(self.scalerX, self.pathFormat(type, 'scalerX'))
+        joblib.dump(self.scalerY, self.pathFormat(type, 'scalerY'))
+
+    def MinMaxScaling(self, type, x, y):
+        # Reset the scalers
+        self.scalerX = MinMaxScaler()
+        self.scalerY = MinMaxScaler()
+
+        # Scale the datasets & convert to numpy arrays
+        x = self.scalerX.fit_transform(numpy.array(x, dtype=float))
+        y = self.scalerY.fit_transform(numpy.array(y, dtype=float))
+
+        self.saveScaler(type)
+
+        # Split into train & test datasets
+        x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.1)
+
+        return x_train, x_test, y_train, y_test
+
+    def LoadDatafile(self):
+        # Load the json file into a pandas dataframe
+        # Drop the filename column
+        # Shuffle the data for good luck
+        #   NOTE: this is not scientifically proven!
+        return shuffle(pandas.read_json('%s/%s' % (self.datadir, self.datafile)).drop(['filename'], axis=1))
+
+    # Load data & return arousal & valence (y) (2,) shape
+    def MinMaxScaledAV(self):
+        data = self.LoadDatafile()
+
+        return self.MinMaxScaling('av', data.iloc[:, 2:], data.iloc[:, :2])
+
+    # Load data & only return arousal (y) (1,) shape
+    def MinMaxScaledArousal(self):
+        data = self.LoadDatafile()
+
+        return self.MinMaxScaling('arousal', data.iloc[:, 2:], data.iloc[:, :1])
+
+    # Load data & only return valence (y) (1,) shape
+    def MinMaxScaledValence(self):
+        data = self.LoadDatafile()
+
+        return self.MinMaxScaling('valence', data.iloc[:, 2:], data.iloc[:, 1:2])
+
+    # Create a keras MES model
+    def MeanSquared(self, x, opt):
+        tensorflow.keras.backend.clear_session()
+
+        model = models.Sequential([
+            #  Create a relu activation layer
+            layers.Dense(39, activation='relu', input_shape=(x.shape[1],), kernel_initializer='normal'),
+
+            # Add a dropout of 25% to prevent overfitting
+            layers.Dropout(.25),
+
+            # Create 3 relu based layers
+            layers.Dense(19, activation='relu'),
+            layers.Dense(9, activation='relu'),
+            layers.Dense(4, activation='relu'),
+
+            # Create the output based on tanh, this makes the output less likely to be around the centre point
+            layers.Dense(1, activation='tanh')
+        ])
+
+        model.compile(optimizer="adam", loss='mean_squared_logarithmic_error', metrics=['mse', 'mae'])
+
+        return model
+
+    # Arousal value refitting, only for plotting purposes
+    def RefitArousalY(self, y):
+        return numpy.insert(numpy.array(y), 0, 0, axis=1)
+
+    # Valence value refitting, only for plotting purposes
+    def RefitValenceY(self, y):
+        return numpy.insert(numpy.array(y), 1, 0, axis=1)
+
+    # Create a arousal based model
+    def CreateArousalModel(self):
+        x_train, x_test, y_train, y_test = self.MinMaxScaledArousal()
+
+        model = self.MeanSquared(x_train, 'adam')
+        history = model.fit(x_train, y_train, epochs=200, verbose=True, validation_data=(x_test, y_test))
+
+        model.save(self.pathFormat('arousal', '.h5'))
+
+        return model
+
+    # Create a valence based model
+    def CreateValenceModel(self):
+        x_train, x_test, y_train, y_test = self.MinMaxScaledValence()
+
+        model = self.MeanSquared(x_train, 'adam')
+        history = model.fit(x_train, y_train, epochs=200, verbose=True, validation_data=(x_test, y_test))
+
+        model.save(self.pathFormat('valence', '.h5'))
+
+        return model
+
+    def LoadModel(self, type):
+        return tensorflow.keras.models.load_model(self.pathFormat(type, '.h5'))
