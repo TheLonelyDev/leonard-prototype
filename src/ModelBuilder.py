4ddc392d81d01083f15fa9ff5f6580ab1e102fad
Bo <bobracquez@gmail.com>
2019-10-13 20:38:35 +0200

    feat(): ModelBuilder
    Created model builder, A & V model building script (simple)

---
+++ src/ModelBuilder.py
@@ -0,0 +1,8 @@
+# ModelBuilder
+# This script is just made to create A & V models once and save them using a dataset
+from src.modules.ModelFactory import ModelFactory
+
+mf = ModelFactory('C:\\Users\\Lonely\\PycharmProjects\\leonard-prototype\\src\\data', 'data4_min.json')
+
+mf.CreateArousalModel()
+mf.CreateValenceModel()
