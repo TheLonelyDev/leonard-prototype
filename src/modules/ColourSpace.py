bf4a665459e51379785721b284a4e2db68137784
Bo <bobracquez@gmail.com>
2019-10-13 21:26:04 +0200

    feat(): use v from hsv & tonicdata

--- src/modules/ColourSpace.py
+++ src/modules/ColourSpace.py
@@ -1,6 +1,10 @@
 import colorsys
 import numpy
 
+def convert(x,a,b,c=0,d=1):
+    """converts values in the range [a,b] to values in the range [c,d]"""
+    return c + float(x-a)*float(d-c)/float(b-a)
+
 class ColourSpace():
     def hsv2rgb(self, h, s, v):
         return tuple(int(round(i * 255)) for i in colorsys.hsv_to_rgb(h, s, v))
@@ -13,7 +17,14 @@ class ColourSpace():
     def fix(self, val):
         return (val + 360) % 360
 
-    def _from_rgb(self, rgb):
+    def from_rgb(self, rgb):
         """translates an rgb tuple of int to a tkinter friendly color code
         """
         return "#%02x%02x%02x" % rgb
+
+    def emotion(self, valence, arousal, scale):
+        x, y = self.cart2pol((valence - .5) * scale, arousal - .5)
+
+        y = self.fix(numpy.degrees(y) + 270)
+        print(arousal, convert(arousal, 0, 1, 0.7, 1))
+        return self.hsv2rgb(y / 360, 1, convert(arousal, 0, 1, 0.5, 1))
